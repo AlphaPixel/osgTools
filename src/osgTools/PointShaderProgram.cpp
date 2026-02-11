d927950658c30f82b80152be4c13197f04c29c1e
John W. Terrell <john@alphapixel.com>
2026-02-11 15:20:56 -0800

    Revert prior change

--- src/osgTools/PointShaderProgram.cpp
+++ src/osgTools/PointShaderProgram.cpp
@@ -9,7 +9,7 @@ const char* vertexShaderSource = R"(
     in vec4 osg_Color;
 
     uniform mat4 osg_ModelViewProjectionMatrix;
-    uniform double pointSize;
+    uniform float pointSize;
 
     out vec4 vertexColor;
 
